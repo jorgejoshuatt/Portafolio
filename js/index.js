3fa28dbdcd81a6c5cb2f33ca1f725143c1f0decc
jorgejoshuatt <joshuatrejo18@aragon.unam.mx>
2023-04-26 01:19:45 -0600

    Apartado de habilidades y educación

--- js/index.js
+++ js/index.js
@@ -1,39 +1,29 @@
-/*
-const carrusel = document.querySelector('.carrusel');
-const flechaIzquierda = document.querySelector('#flecha-izquierda');
-const flechaDerecha = document.querySelector('#flecha-derecha');
-let indice = 0;
-
-const items = carrusel.querySelectorAll('.item');
-const numItems = items.length;
-
-// Establecer la clase active para los primeros 3 elementos
-for (let i = 0; i < 3; i++) {
-  items[i].classList.add('active');
-}
-
-flechaDerecha.addEventListener('click', () => {
-  if (indice < numItems - 3) {
-    // Ocultar el elemento que sale de la vista
-    items[indice].classList.remove('active');
-    // Mostrar el elemento que entra en la vista
-    items[indice + 3].classList.add('active');
-    // Mover el carrusel
-    carrusel.style.left  = `-${(indice + 1) * 100 / 3}%`;
-    indice++;
-  }
+const items = document.querySelectorAll('.item');
+const cuerpos = document.querySelectorAll('.cuerpo');
+const sobreMi = document.querySelector('.sobre__mi');
+const cuerpoSobreMi = document.querySelector('.cuerpo.sobre__mi');
+sobreMi.classList.add('active');
+cuerpoSobreMi.classList.add('active');
+items.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    items.forEach(item => item.classList.remove('active'));
+    cuerpos.forEach(cuerpo => cuerpo.classList.remove('active'));
+    item.classList.add('active');
+    cuerpos[index].classList.add('active');
+  });
 });
 
-flechaIzquierda.addEventListener('click', () => {
-  if (indice > 0) {
-    // Ocultar el elemento que sale de la vista
-    items[indice + 2].classList.remove('active');
-    // Mostrar el elemento que entra en la vista
-    items[indice - 1].classList.add('active');
-    // Mover el carrusel
-    carrusel.style.right = `-${(indice - 1) * 100 / 3}%`;
-    indice--;
-  }
-});
-
-*/
+const opciones = document.querySelectorAll('.opcion');
+const descripciones = document.querySelectorAll('.descripcion');
+const educacion = document.querySelector('.opcion.educacion');
+const descripcionEducacion = document.querySelector('.descripcion.educacion');
+educacion.classList.add('active');
+descripcionEducacion.classList.add('active');
+opciones.forEach((opcion, index) => {
+    opcion.addEventListener('click', () => {
+      opciones.forEach(opcion => opcion.classList.remove('active'));
+      descripciones.forEach(descripcion => descripcion.classList.remove('active'));
+      opcion.classList.add('active');
+      descripciones[index].classList.add('active');
+    });
+  });
