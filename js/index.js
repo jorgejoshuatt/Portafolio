66fc91777786d6c977595059411528f123b535a1
Josh Trejo <joshuatrejo18@aragon.unam.mx>
2023-05-13 01:22:26 -0600

    Portafolio completado y urls

--- js/index.js
+++ js/index.js
@@ -20,10 +20,40 @@ const descripcionEducacion = document.querySelector('.descripcion.educacion');
 educacion.classList.add('active');
 descripcionEducacion.classList.add('active');
 opciones.forEach((opcion, index) => {
-    opcion.addEventListener('click', () => {
-      opciones.forEach(opcion => opcion.classList.remove('active'));
-      descripciones.forEach(descripcion => descripcion.classList.remove('active'));
-      opcion.classList.add('active');
-      descripciones[index].classList.add('active');
-    });
+  opcion.addEventListener('click', () => {
+    opciones.forEach(opcion => opcion.classList.remove('active'));
+    descripciones.forEach(descripcion => descripcion.classList.remove('active'));
+    opcion.classList.add('active');
+    descripciones[index].classList.add('active');
   });
+});
+
+const opcionesSlider = document.querySelectorAll('.opcion__slider');
+const contenidosSlider = document.querySelectorAll('.contenido__slider');
+const sliderUno = document.querySelector('.opcion__slider.uno');
+const contenidoSliderUno = document.querySelector('.contenido__slider.uno');
+sliderUno.classList.add('active');
+contenidoSliderUno.classList.add('active');
+opcionesSlider.forEach((opcionSlider, index) => {
+  opcionSlider.addEventListener('click', () => {
+    opcionesSlider.forEach(opcionSlider => opcionSlider.classList.remove('active'));
+    contenidosSlider.forEach(contenidoSlider => contenidoSlider.classList.remove('active'));
+    opcionSlider.classList.add('active');
+    contenidosSlider[index].classList.add('active');
+  });
+});
+
+const opcionesSliderCertificados = document.querySelectorAll('.opcion__slider__certificados');
+const contenidosSliderCertificados = document.querySelectorAll('.contenido__slider__certificados');
+const sliderUnoCertificado = document.querySelector('.opcion__slider__certificados.uno');
+const contenidoSliderUnoCertificado = document.querySelector('.contenido__slider__certificados.uno');
+sliderUnoCertificado.classList.add('active');
+contenidoSliderUnoCertificado.classList.add('active');
+opcionesSliderCertificados.forEach((opcionSlider, index) => {
+  opcionSlider.addEventListener('click', () => {
+    opcionesSliderCertificados.forEach(opcionSlider => opcionSlider.classList.remove('active'));
+    contenidosSliderCertificados.forEach(contenidoSlider => contenidoSlider.classList.remove('active'));
+    opcionSlider.classList.add('active');
+    contenidosSliderCertificados[index].classList.add('active');
+  });
+});
